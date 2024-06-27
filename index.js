5fd35634b9971a3829d3c62e708d7bcff975ff40
ESTON NDUNGU <estonndungu1@gmail.com>
2024-06-27 12:52:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+//Declare  variable in global scope 
+var customerName = "bob"
+
+// Modifies variable to uppercase
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+// Sets best customer
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+// Overwrites the best customer
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavouriteCustomer = "Rob"
+
+// Does not reassign the least favorite customer
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = "Cain"
+
+}
+
+
